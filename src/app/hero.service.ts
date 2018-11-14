23e42040189b47bb79bd59c7f3c29647035f4aad
Andrew Bateman <gomezbateman@yahoo.com>
2018-11-14 12:04:08 +0100

    app complete and working

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -33,23 +33,67 @@ export class HeroService {
       	catchError(this.handleError('getHeroes', []))
     	);
 	}
-
-	/** Log a HeroService message with the MessageService */
-	private log(message: string) {
-  	this.messageService.add(`HeroService: ${message}`);
+	
+  /** GET hero by id. Will 404 if id not found */
+	getHero(id: number): Observable<Hero> {
+  	const url = `${this.heroesUrl}/${id}`;
+  	return this.http.get<Hero>(url).pipe(
+    	tap(_ => this.log(`fetched hero id=${id}`)),
+    	catchError(this.handleError<Hero>(`getHero id=${id}`))
+  	);
 	}
 
-	
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 
+	//////// Save methods //////////
 
-	/**
+	/** POST: add a new hero to the server */
+	addHero (hero: Hero): Observable<Hero> {
+  	return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+    	tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+    	catchError(this.handleError<Hero>('addHero'))
+  	);
+	}
+
+	/** DELETE: delete the hero from the server */
+	deleteHero (hero: Hero | number): Observable<Hero> {
+  	const id = typeof hero === 'number' ? hero : hero.id;
+  	const url = `${this.heroesUrl}/${id}`;
+
+  	return this.http.delete<Hero>(url, httpOptions).pipe(
+    	tap(_ => this.log(`deleted hero id=${id}`)),
+    	catchError(this.handleError<Hero>('deleteHero'))
+  	);
+	}
+
+	/** PUT: update the hero on the server
+	The HttpClient.put() method takes three parameters:
+	the URL, the data to update & options	*/
+	updateHero (hero: Hero): Observable<any> {
+  	return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+    	tap(_ => this.log(`updated hero id=${hero.id}`)),
+    	catchError(this.handleError<any>('updateHero'))
+  	);
+	}
+
+ /*
  * Handle Http operation that failed.
  * Let the app continue.
  * @param operation - name of the operation that failed
  * @param result - optional value to return as the observable result
  */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+	private handleError<T> (operation = 'operation', result?: T) {
+  	return (error: any): Observable<T> => {
  
     // TODO: send the error to remote logging infrastructure
     console.error(error); // log to console instead
@@ -59,57 +103,13 @@ private handleError<T> (operation = 'operation', result?: T) {
  
     // Let the app keep running by returning an empty result.
     return of(result as T);
-  };
-}
-
-/** GET hero by id. Will 404 if id not found */
-	getHero(id: number): Observable<Hero> {
-  	const url = `${this.heroesUrl}/${id}`;
-  	return this.http.get<Hero>(url).pipe(
-    	tap(_ => this.log(`fetched hero id=${id}`)),
-    	catchError(this.handleError<Hero>(`getHero id=${id}`))
-  	);
-	}
-	/** PUT: update the hero on the server
-	The HttpClient.put() method takes three parameters:
-	the URL, the data to update & options	*/
-
-	updateHero (hero: Hero): Observable<any> {
-  	return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
-    	tap(_ => this.log(`updated hero id=${hero.id}`)),
-    	catchError(this.handleError<any>('updateHero'))
-  	);
+  	};
 	}
 
-	/** POST: add a new hero to the server */
-	addHero (hero: Hero): Observable<Hero> {
-  	return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-    	tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
-    	catchError(this.handleError<Hero>('addHero'))
-  	);
+	/** Log a HeroService message with the MessageService */
+	private log(message: string) {
+  	this.messageService.add(`HeroService: ${message}`);
 	}
-	/** DELETE: delete the hero from the server */
-deleteHero (hero: Hero | number): Observable<Hero> {
-  const id = typeof hero === 'number' ? hero : hero.id;
-  const url = `${this.heroesUrl}/${id}`;
-
-  return this.http.delete<Hero>(url, httpOptions).pipe(
-    tap(_ => this.log(`deleted hero id=${id}`)),
-    catchError(this.handleError<Hero>('deleteHero'))
-  );
-}
-/* GET heroes whose name contains search term */
-searchHeroes(term: string): Observable<Hero[]> {
-  if (!term.trim()) {
-    // if not search term, return empty hero array.
-    return of([]);
-  }
-  return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
-    tap(_ => this.log(`found heroes matching "${term}"`)),
-    catchError(this.handleError<Hero[]>('searchHeroes', []))
-  );
-}
-  
 }
 /*An observable is an array whose items arrive asynchronously over time. 
 Observables help you manage asynchronous data, such as data coming from 
